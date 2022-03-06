72be38f5449e803c6c2746e78400adf432fb9417
Alex <ajrey@live.co.uk>
2022-03-06 00:07:07 +0000

    Added weird don and 8ball

--- index.mjs
+++ index.mjs
@@ -1,6 +1,14 @@
 //Importing packages for both discordjs and dotenv from their node installs
-import DiscordJS, { Client, Intents, Message } from "discord.js";
+import DiscordJS, {
+    Client,
+    Intents,
+    Message,
+    MessageAttachment,
+} from "discord.js";
 import dotenv from "dotenv";
+var fs = import("fs");
+import { readdir } from "fs/promises";
+import { readdirSync } from "fs";
 
 //Allows for accesses to whats stored in the dotenv file
 dotenv.config();
@@ -14,6 +22,7 @@ const client = new DiscordJS.Client({
 //When the bot is ready it will console log it
 client.on("ready", () => {
     console.log("The bot is ready");
+    const attachment = new MessageAttachment("https://i.imgur.com/XxxXxXX.jpg");
 });
 
 client.on("messageCreate", (message) => {
@@ -23,6 +32,20 @@ client.on("messageCreate", (message) => {
                 Date.now() - message.createdTimestamp
             }ms \n API Latency: ${Math.round(client.ws.ping)}ms`,
         });
+    }
+    if (message.content.toLowerCase().includes("don")) {
+        const dondir = "./images/dons";
+        const length = readdirSync(dondir).length;
+
+        const num = Math.floor(Math.random() * length);
+        message.channel.send({ files: [`./images/dons/${num}.jpg`] });
+    }
+    if (message.content.toLowerCase().includes("8ball")) {
+        const balldir = "./images/8ball";
+        const length = readdirSync(balldir).length;
+
+        const num = Math.floor(Math.random() * length);
+        message.channel.send({ files: [`./images/8ball/${num}.gif`] });
     }
 });
 
