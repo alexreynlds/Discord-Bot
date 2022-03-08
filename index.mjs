441668af4a6c692fa012e224dc6b16bdccb07e30
Alex <ajrey@live.co.uk>
2022-03-08 16:29:44 +0000

    started music bot functionality

--- index.mjs
+++ index.mjs
@@ -4,19 +4,36 @@ import DiscordJS, {
     Intents,
     Message,
     MessageAttachment,
+    MessageEmbed,
 } from "discord.js";
+import {
+    AudioPlayerStatus,
+    AudioResource,
+    entersState,
+    joinVoiceChannel,
+    VoiceConnectionStatus,
+    createAudioPlayer,
+    createAudioResource,
+} from "@discordjs/voice";
 import dotenv from "dotenv";
 var fs = import("fs");
 import { readdir } from "fs/promises";
 import { readdirSync } from "fs";
+import { channel } from "diagnostics_channel";
 
 //Allows for accesses to whats stored in the dotenv file
 dotenv.config();
 
+const player = createAudioPlayer();
 //Create a new instance of a discord client
 const client = new DiscordJS.Client({
     //In discordjs 13 you must state the bots intentions
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_INVITES,
+        Intents.FLAGS.GUILD_VOICE_STATES,
+    ],
 });
 
 //When the bot is ready it will console log it
@@ -34,18 +51,39 @@ client.on("messageCreate", (message) => {
         });
     }
     if (message.content.toLowerCase().includes("don")) {
-        const dondir = "./images/dons";
+        const dondir = "./content/images/dons";
         const length = readdirSync(dondir).length;
 
         const num = Math.floor(Math.random() * length);
-        message.channel.send({ files: [`./images/dons/${num}.jpg`] });
+        message.channel.send({ files: [`./content/images/dons/${num}.jpg`] });
     }
     if (message.content.toLowerCase().includes("8ball")) {
         const balldir = "./images/8ball";
         const length = readdirSync(balldir).length;
 
         const num = Math.floor(Math.random() * length);
-        message.channel.send({ files: [`./images/8ball/${num}.gif`] });
+        message.channel.send({ files: [`./content/images/8ball/${num}.gif`] });
+    }
+    //Doesn't work! come back to later!
+    if (message.content.toLowerCase() == "cinv") {
+        let channel = message.channel;
+        let invite = channel.createInvite({ unique: true });
+        message.reply(`Invite code: ${invite.code}`);
+    }
+    if (message.content.toLowerCase() == "joinvc") {
+        joinVoiceChannel({
+            channelId: message.member.voice.channel.id,
+            guildId: message.guild.id,
+            adapterCreator: message.guild.voiceAdapterCreator,
+        });
+    }
+    if (message.content.toLowerCase() == "playvc") {
+        const resource = createAudioResource(`./content/audio/urmom.mp3`);
+        player.play(resource);
+
+        player.on("error", (error) => {
+            console.error(error);
+        });
     }
 });
 
